b2477581f35dfd618aa28ad34867441872384aef
Manendra Singh Rawat <mannuatnri.itm@gmail.com>
2023-08-04 17:52:13 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *          <h1>Hello React</h1>
- *          <h2>Hello React</h2>
- *       </div>
- * </div>
- */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
   "div",
